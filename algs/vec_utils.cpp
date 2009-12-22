198cc8471cbacdb4ea6faeac880051721ac2d8f4
Iulian Grindeanu <iulian@mcs.anl.gov>
2009-12-22 15:45:06 +0000

    Project Shell algorithm    input file is a shell triangular mesh, manifold    output is a projected intersected mesh in 2d;    by default, the nodes that are 1.e-5 apart are merged     triangles that have an area 1.e-10 are collapsed too    The algorithm starts by determining the orientation of triangles after     projection. It divides them in 2 groups. It also assumes that they are    oriented correctly (some checks will be made, for manifold mesh and   orientation)   It uses an advancing front method, by pairing each triangle from one group (negative,    blue) to one triangle from the other group (positive, red).   For each pair, (red A, blue B) , all red triangles that intersect B are found by     looking at the neighbors of the initial red triangle A.   The patches that result from intersection of any 2 triangles are strictly convex, and    are decomposed easily in triangles   The algorithm keeps also a list of intersection nodes on the red edges, collapsing them    if necessary.

--- algs/vec_utils.cpp
+++ algs/vec_utils.cpp
@@ -22,7 +22,21 @@ double dist(const double *a)
 double * normalize(double *res, const double *a)
 {
   double d = dist(a);
+  if (0.==d)// no division with 0
+     d=1.;
   for(int i=0; i<3; i++)
     res[i] = a[i]/d;
   return res;
+}
+
+// distance in 2D
+double dist2(const double *a, const double *b)
+{
+  return std::sqrt((b[0]-a[0])*(b[0]-a[0])+(b[1]-a[1])*(b[1]-a[1]));
+}
+// area of a triangle (with sign)
+double area2D(const double * a, const double * b, const double *c) // could be negative or 0!
+{
+  double area = (b[0]-a[0])*(c[1]-a[1])-(c[0]-a[0])*(b[1]-a[1]);
+  return area/2;
 }
--- algs/vec_utils.hpp
+++ algs/vec_utils.hpp
@@ -5,5 +5,7 @@ double * cross(double *res, const double *a, const double *b);
 double dot(const double *a, const double *b);
 double dist(const double *a);
 double * normalize(double *res, const double *a);
+double dist2(const double *a, const double * b);
+double area2D(const double * a, const double * b, const double *c); // could be negative or 0!
 
 #endif
