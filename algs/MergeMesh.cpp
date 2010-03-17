c0ba8b515c3bd310f788ed127b1f678753742da3
MeshKit <meshkit-dev@mcs.anl.gov>
2010-03-17 19:56:50 +0000

    o namespace moab is added

--- algs/MergeMesh.cpp
+++ algs/MergeMesh.cpp
@@ -7,6 +7,8 @@
 #include "MBCartVect.hpp"
 #endif
 
+using namespace moab;
+
 int MergeMesh::merge_entities(iBase_EntityHandle *elems,
                               int elems_size,
                               const double merge_tol,
