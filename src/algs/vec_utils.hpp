9e5505ec6759040ecff4504d81472fa446fdaaf8
MeshKit <meshkit-dev@mcs.anl.gov>
2011-02-07 21:30:59 +0000

    Moved ProjectShell and dependencies around to repackage using the new design.

---
+++ src/algs/vec_utils.hpp
@@ -0,0 +1,14 @@
+#ifndef VEC_UTILS_H
+#define VEC_UTILS_H
+
+double * cross(double *res, const double *a, const double *b);
+double dot(const double *a, const double *b);
+double dist(const double *a);
+double distP(const double *a, const double *b);
+double * normalize(double *res, const double *a);
+double dist2(const double *a, const double * b);
+double area2D(const double * a, const double * b, const double *c); // could be negative or 0!
+
+double normal3D(double * normal, const double *a, const double *b, const double *c);
+
+#endif
