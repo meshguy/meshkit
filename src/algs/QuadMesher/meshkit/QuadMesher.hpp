45df342de022f9fd516c3f9a018aa5f6f86f3aa4
MeshKit <meshkit-dev@mcs.anl.gov>
2011-02-19 03:30:15 +0000

    QuadMesher header file added...

---
+++ src/algs/QuadMesher/meshkit/QuadMesher.hpp
@@ -0,0 +1,80 @@
+//-----------------------------------C++-------------------------------------//
+// File: src/algs/QuadMesher.hpp
+// Wednesday February 11 10:50 2011
+// Brief: QuadMesher class definition: four schemes are provided: equal meshing,
+//        Bias Meshing, Dual Bias Meshing, Curvature-based meshing 
+//---------------------------------------------------------------------------//
+
+#ifndef __QUADMESHER_H
+#define __QUADMESHER_H
+
+#include "meshkit/iGeom.hpp"
+#include "meshkit/MeshScheme.hpp"
+#include "Tri2Quad.hpp"
+
+namespace MeshKit
+{
+//===========================================================================//
+  /*!
+   * \class QuadMesher
+   * \brief Triangle to Quad Transformation.
+   * 
+   */
+//===========================================================================//
+
+using namespace std;
+
+class QuadMesher : public MeshScheme
+{
+
+public:
+	//construction function for edge mesher
+	QuadMesher(MKCore *mk_core, const MEntVector &me_vec);
+
+	//set up the parameters for edge meshing, e.g. compute the number of intervals
+	virtual void setup_this();
+
+	//Generate the edge mesh
+	virtual void execute_this();
+
+       /**\brief Get class name */
+       static const char* name() { return "QuadMesher"; }
+
+       /**\brief Function returning whether this scheme can mesh entities of t
+        *        the specified dimension.
+        *\param dim entity dimension
+        */
+       static bool can_mesh(iBase_EntityType dim)
+          { return iBase_FACE == dim; }
+
+       /** \brief Function returning whether this scheme can mesh the specified entity
+        * 
+        * Used by MeshOpFactory to find scheme for an entity.
+        * \param me ModelEnt being queried
+        * \return If true, this scheme can mesh the specified ModelEnt
+        */
+       static bool can_mesh(ModelEnt *me)
+          { return canmesh_face(me); }
+
+
+       /**\brief Get list of mesh entity types that can be generated.
+        *\return array terminated with \c moab::MBMAXTYPE
+        */
+       static const moab::EntityType* output_types();
+
+       /** \brief Return the mesh entity types operated on by this scheme
+        * \return array terminated with \c moab::MBMAXTYPE
+        */
+       virtual const moab::EntityType* mesh_types_arr() const
+         { return output_types(); }
+
+	~QuadMesher();
+	
+private:
+      void tri2quad_conversion( ModelEnt *me);
+      Jaal::Mesh* QuadMesher :: tri_quad_conversion (Jaal::Mesh *trimesh);
+};
+
+}
+
+#endif
