22128177277fff2d1f787bcfc65caa2de62771fc
MeshKit <meshkit-dev@mcs.anl.gov>
2011-02-28 18:30:05 +0000

    some changes to the section on adding a new algs/ subdir

--- doc/faq.h
+++ doc/faq.h
@@ -1,7 +1,7 @@
 /*!
   \page faq Frequently Asked Questions
  - \ref newdir "I'm implementing a new algorithm; should I put the source files directly in src/algs, or add a new subdirectory below that?"\n
- - \ref buildingnewdir "How do I add a new subdirectory, e.g. a new mesher with multiple files, to the %%MeshKit source?"
+ - \ref buildingnewdir "How do I add a new subdirectory, e.g. a new mesher with multiple files, to the &MeshKit source?"
 
 \anchor newdir <b>I'm implementing a new algorithm; should I put the source files directly in src/algs, or add a new 
 subdirectory below that?</b>\n
@@ -18,13 +18,22 @@ these are the headers that contain the API to your new mesher.
  -# In the top-level configure.ac file, just before the end of the file, add a line
 to create the src/algs/myalg/Makefile (make it look like the others).
  -# In src/algs/Makefile.am, add myalg to the list of directories assigned to SUBDIRS
- -# In src/algs/myalg, create a Makefile.am, by copying the one from src/algs/Makefile.am
- -# Everywhere you see lib%MeshKitalgs, substitute your own library name, e.g. lib%MeshKitmyalg
- -# In the variable nobase_lib%MeshKitalgs_la_include_HEADERS, substitute the names of all the files you put in
+ -# In src/algs/Makefile.am, add your library to the LIBADD list for
+     libMeshkitalgs.la
+ -# Add your directory to the AM_CPPFLAGS in src/algs/Makfile.am so that
+     you can include your algorithm in src/algs/register_algs.cpp
+ -# Add an entry to src/algs/register_algs.cpp to register your algorithm
+     with MKCore. (See \ref newmeshop).
+ -# In src/algs/myalg, create a Makefile.am.  It is probably easiest to
+    copy an existing one from elsewhere (e.g. some other subdir of algs/).
+ -# Append to AM_CPPFLAGS the list of places that you expect the compiler
+    to search for header files.  Keep in mind that for building in a separate
+    directory from the source directory to work you will need to prefix
+    paths with \$(srcdir)/.
+ -# In the variable nobase_libMeshKitmyalg_la_include_HEADERS, substitute the names of all the files you put in
 src/algs/myalg/meshkit
- -# Put the names of all the files you put in src/algs/myalg in nobase_lib%MeshKitalgs_la_include_SOURCES (including
+ -# Put the names of all the files you put in src/algs/myalg in libMeshKitmyalg_la_SOURCES (including
 the header files there)
- -# Add the new directory src/algs/myalg to the AM_CPPFLAGS in src/algs/myalg/Makefile.am (with a -I in front of it)
  -# In the top-level %MeshKit directory, run autoreconf -fi
  -# Run make; the code in myalg should build
 
