d87dc9219538c5602cd3a379732f4b1fdd713f9c
Iulian Grindeanu <iulian@mcs.anl.gov>
2011-06-17 19:31:52 +0000

    constrain edges in all faces

--- src/core/MeshScheme.cpp
+++ src/core/MeshScheme.cpp
@@ -10,10 +10,12 @@ void MeshScheme::constrain_even()
     // constrain all edges to be even
   MEntVector edges;
   for (MEntSelection::iterator sit = mentSelection.begin(); sit != mentSelection.end(); sit++)
+  {
     (*sit).first->get_adjacencies(1, edges);
-    
-  for (MEntVector::iterator vit = edges.begin(); vit != edges.end(); vit++) 
-    (*vit)->constrain_even(true);
+
+    for (MEntVector::iterator vit = edges.begin(); vit != edges.end(); vit++)
+      (*vit)->constrain_even(true);
+  }
 }
 
 } // namespace MeshKit
