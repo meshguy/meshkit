b0e216be60abf2136d2b2668ea0310fbad21dd5d
MeshKit <meshkit-dev@mcs.anl.gov>
2011-07-07 21:14:43 +0000

    Update examples

--- python/examples/onetooneswept.py
+++ python/examples/onetooneswept.py
@@ -1,31 +1,45 @@
 from MeshKit import *
 
-file_name = "../data/BrickWithSrcMeshed.cub"
+parser = OptionParser(usage="Usage: %prog [options] [FILE]")
+parser.add_option("-s", "--save", dest="save",
+                  help="(optional) filename to save")
+
+options, args = parser.parse_args()
+
+if len(args) == 0:
+    file_name = "../data/BrickWithSrcMeshed.cub"
+elif len(args) == 1:
+    file_name = args[0]
+else:
+    print parser.get_usage()
+
+
 mk = MKCore()
 mk.load_geometry_mesh(file_name, file_name)
 
 vols = MEntVector()
 mk.get_entities_by_dimension(3, vols)
 
-this_vol = vols[0]
-
 surfs = MEntVector()
-this_vol.get_adjacencies(2, surfs)
+vols[0].get_adjacencies(2, surfs)
 
 curves = MEntVector()
-this_vol.get_adjacencies(1, curves)
+vols[0].get_adjacencies(1, curves)
 
 vertices = MEntVector()
-this_vol.get_adjacencies(0, vertices)
+vols[0].get_adjacencies(0, vertices)
 
-sw = mk.construct_meshop("OneToOneSwept", vols)
+first_vol = MEntVector()
+first_vol.push_back(vols.front())
+sw = mk.construct_meshop("OneToOneSwept", vols[0])
 
 sw.SetSourceSurface(1)
 sw.SetTargetSurface(0)
 
 swSize = SizingFunction(mk, 6, -1)
-this_vol.sizing_function_index(swSize.core_index())
+vols[0].sizing_function_index(swSize.core_index())
 
 mk.setup_and_execute()
 
-mk.save_mesh("OneToOneSwept.vtk")
+if options.save:
+    mk.save_mesh(options.save)
