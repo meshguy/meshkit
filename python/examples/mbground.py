32e37332a47b5db4c2e3cbdc142b9d502fdaf071
Iulian Grindeanu <iulian@mcs.anl.gov>
2012-02-25 22:10:01 +0000

    added another parameter to MBSplitOp::set_options that need to be passed to python, too (rev 4317 )

--- python/examples/mbground.py
+++ python/examples/mbground.py
@@ -17,7 +17,8 @@ direction = [0.,  0.,  1. ]
 poly = [537200,  7680200,  -2000., 537800,  7680100,  -2000., 537700,  7680980,  -2000.,  537400 , 7680930 , -2000. ,537100 ,7680900 , -2000.]
 gid = 1
 closed = 0
-qm.set_options(gid, direction[0], direction[1], direction[2], closed)
+min_dot = 0.8
+qm.set_options(gid, direction[0], direction[1], direction[2], closed, min_dot)
 qm.add_points(537000  , 7680445,  150.8383)
 qm.add_points(537500 ,  7680300  , -2000.)
 qm.add_points(537700 , 7680400 , -2000.)
