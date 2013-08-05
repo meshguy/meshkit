aa1839e8479b45dfa94796f6814566aa93d380d1
MeshKit <meshkit-dev@mcs.anl.gov>
2013-08-05 19:46:19 +0000

    Possible template for new algorithms page

--- doc/userguide/meshkitalgorithms.h
+++ doc/userguide/meshkitalgorithms.h
@@ -3,6 +3,151 @@
 
 This is a temporary page to clear up the sidebar, it will eventually replace the old algorithms page.\n
 
+\htmlonly
+
+<center>
+<table width=80% bgcolor=deepskyblue style="text-align: center;" border="0" cellpadding="5" cellspacing="2">
+
+<tbody><tr>
+<td> Key: </td>
+<td bgcolor=green> LGPL-compatible license <br> Bundled with MeshKit </td>
+<td bgcolor=yellow> LGPL-compatible license <br> External </td>
+<td bgcolor=red> Proprietary or GPL license <br> External </td>
+</tr></tbody></table>
+</center>
+
+<br><br><big><big>  Mesh Generation Algorithms  </big></big><br><br>
+
+<center><table width=80% bgcolor=deepskyblue style="text-align: center;" border="0" cellpadding="5" cellspacing="2"><tbody>
+
+<tr> <bgcolor=gray> <td>
+Algorithm</td><td>
+Related Examples</td><td>
+Related Tests</td><td>
+Brief Info</td></tr>
+
+<tr bgcolor=white> <td colspan="4"> <big><b>0D Algorithms</b></big> </td> </tr>
+
+<tr bgcolor=springgreen>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+this goign to be in no way considered brief by anyone who would ever be evaluating whether this sentence is brief or not Generates Quads                </td></tr>
+
+<tr bgcolor=goldenrod>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=skyblue>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Generates Tets                </td></tr>
+
+<tr bgcolor=white> <td colspan="4"> <big><b>1D Algorithms</b></big> </td> </tr>
+
+<tr bgcolor=cyan>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=white> <td colspan="4"> <big><b>2D Algorithms</b></big> </td> </tr>
+
+<tr bgcolor=green>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=red>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=yellow>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=white> <td colspan="4"> <big><b>3D Algorithms</b></big> </td> </tr>
+
+<tr bgcolor=yellow>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=green>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=yellow>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=cyan>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+<tr bgcolor=cyan>                         <td> \endhtmlonly
+\subpage algorithm_VertexMesher         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+</tbody></table></center>
+
+<br><br><big><big>  Geometry Algorithms Algorithms  </big></big><br><br>
+
+<center><table width=80% bgcolor=deepskyblue style="text-align: center;" border="0" cellpadding="5" cellspacing="2"><tbody>
+
+<tr bgcolor=white> <td>
+Algorithm</td><td>
+Related Examples</td><td>
+Related Tests</td><td>
+Brief Info</td></tr>
+
+<tr bgcolor=cyan>                         <td> \endhtmlonly
+\subpage algorithm_MeshOpTemplate         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+</tbody></table></center>
+
+
+<br><br><big><big>  Mesh Modification Algorithms  </big></big><br><br>
+
+<center><table width=80% bgcolor=deepskyblue style="text-align: center;" border="0" cellpadding="5" cellspacing="2"><tbody>
+
+<tr bgcolor=white> <td>
+Algorithm</td><td>
+Related Examples</td><td>
+Related Tests</td><td>
+Brief Info</td></tr>
+
+<tr bgcolor=cyan>                         <td> \endhtmlonly
+\subpage algorithm_MeshOpTemplate         \htmlonly </td> <td> \endhtmlonly
+\ref basic.cpp                            \htmlonly </td> <td> \endhtmlonly
+\ref test_meshoptemplate.cpp              \htmlonly </td> <td>
+Creates rectangular prisms                </td></tr>
+
+</tbody></table></center>
+
+\endhtmlonly
+
+
 \subpage algorithm_AssyGen
 \subpage algorithm_CAMALTetMesher
 \subpage algorithm_CAMALTriAdvance
@@ -20,5 +165,9 @@ This is a temporary page to clear up the sidebar, it will eventually replace the
 \subpage algorithm_QslimMesher
 \subpage algorithm_SCDMesh
 \subpage algorithm_VertexMesher
+
+Geometry Algorithms
+
+Mixed Algorithms
 
 */
