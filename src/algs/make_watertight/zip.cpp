b9f68282326a47abc0f171bb426c91a74bf78740
Jacob Becker <jacob.becker@kitware.com>
2014-07-28 12:00:16 -0400

    fix a build issue with osx

--- src/algs/make_watertight/zip.cpp
+++ src/algs/make_watertight/zip.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "meshkit/zip.hpp"
 #include "MBOrientedBoxTreeTool.hpp"
 
@@ -33,7 +34,7 @@ namespace zip {
     //std::cout << "t_joint: tris.size()=" << tris.size() << std::endl;
     //MBI()->list_entities( tris );
 
-    triangles joints[tris.size()];
+    std::vector<triangles> joints(tris.size());
     for(unsigned int i=0; i<tris.size(); i++) {
       joints[i].before_tri = tris[i];
 
