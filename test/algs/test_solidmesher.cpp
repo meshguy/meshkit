a0df65c3255dfe3e76f3f9c0a601e7c6f15aa747
pshriwise <shriwise@wisc.edu>
2014-08-22 13:10:28 -0500

    Added a test for the vertex positions in test_solidmesher.

--- test/algs/test_solidmesher.cpp
+++ test/algs/test_solidmesher.cpp
@@ -27,6 +27,7 @@ void read_cube_tris_test();
 void read_cube_curves_test();
 void read_cube_surfs_test();
 void read_cube_vols_test();
+void read_cube_vertex_pos_test();
 
 int count_topo_for_dim( int dim, iMesh::EntityTopology topo);
 
@@ -61,6 +62,7 @@ int main(int argc, char **argv)
   num_fail += RUN_TEST(read_cube_curves_test);
   num_fail += RUN_TEST(read_cube_surfs_test);
   num_fail += RUN_TEST(read_cube_vols_test);
+  num_fail += RUN_TEST(read_cube_vertex_pos_test);
 
 #if HAVE_OCC
   return 0;
@@ -126,6 +128,105 @@ void read_cube_vols_test()
   CHECK_EQUAL(1, num_of_surfs);
 }
 
+void read_cube_vertex_pos_test()
+{
+
+  MEntVector ents;
+
+  mk->get_entities_by_dimension(0, ents);
+
+  int num_of_verts = ents.size();
+
+  // should be 8 vertex model ents for the cube
+  CHECK_EQUAL(8, num_of_verts);
+
+  // get the vertex coordinates
+  double x[8];
+  double y[8];
+  double z[8];
+
+  for(unsigned int i = 0; i < ents.size(); i++)
+    {
+      //get the iMesh EntityHandle for the vert
+      iMesh::EntitySetHandle sh = IBSH(ents[i]->mesh_handle());
+
+      std::vector<iMesh::EntityHandle> vert;
+      mk->imesh_instance()->getEntities(sh, iBase_ALL_TYPES, iMesh_ALL_TOPOLOGIES, vert);
+
+      int vert_size = vert.size();
+      // there should only be one entity in this set. If not, there is a problem.
+      assert(1 == vert_size);
+  
+      //now get the vertex coords      
+      mk->imesh_instance()->getVtxCoord(vert[0], x[i], y[i], z[i]);
+    }
+
+  //Check against known locations of the vertices
+
+  std::vector<double> x_ref;
+  std::vector<double> y_ref;
+  std::vector<double> z_ref;
+
+  // Vertex 1
+  x_ref.push_back( 5 );
+  y_ref.push_back( -5 );
+  z_ref.push_back( 5 );
+
+  // Vertex 2
+  x_ref.push_back( 5 );
+  y_ref.push_back( 5 );
+  z_ref.push_back( 5 );
+
+  // Vertex 3
+  x_ref.push_back( -5 );
+  y_ref.push_back( 5 );
+  z_ref.push_back( 5 );
+
+  // Vertex 4
+  x_ref.push_back( -5 );
+  y_ref.push_back( -5 );
+  z_ref.push_back( 5 );
+
+  // Vertex 5
+  x_ref.push_back( 5 );
+  y_ref.push_back( 5 );
+  z_ref.push_back( -5 );
+
+  // Vertex 6
+  x_ref.push_back( 5 );
+  y_ref.push_back( -5 );
+  z_ref.push_back( -5 );
+
+  // Vertex 7
+  x_ref.push_back( -5 );
+  y_ref.push_back( -5 );
+  z_ref.push_back( -5 );
+ 
+  // Vertex 8
+  x_ref.push_back( -5 );
+  y_ref.push_back( 5 );
+  z_ref.push_back( -5 );
+
+  
+  for(unsigned int i=0; i<ents.size(); i++)
+    {
+      for(unsigned int j=0; j<x_ref.size(); j++)
+	{
+	  if( x[i]==x_ref[j] && y[i]==y_ref[j] && z[i]==z_ref[j] )
+            {
+              x_ref.erase( x_ref.begin()+j );
+              y_ref.erase( y_ref.begin()+j );
+              z_ref.erase( z_ref.begin()+j );
+            }
+	}
+    }
+  
+  //After looping through each vertex loaded from the mesh
+  //there should be no entities left in the reference vector
+  int leftovers = x_ref.size();
+  CHECK_EQUAL( 0, leftovers );
+
+}
 
 int count_topo_for_dim( int dim, iMesh::EntityTopology topo)
 {
