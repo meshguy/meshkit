71cbf040710dcd08fbc936bba89777039bbe2c68
pshriwise <shriwise@wisc.edu>
2015-07-01 14:14:43 -0500

    Added documentation files for the SolidMesher classes.

---
+++ doc/algs/algorithm_SolidCurveMesher.h
@@ -0,0 +1,25 @@
+/*!
+ \page algorithm_solidcurvemesher SolidCurveMesher
+
+<b>Name:</b> SolidCurveMesher
+
+<b>External dependencies:</b> (none)
+
+<b>Input:</b> 1D ModelEnt's
+
+<b>Output:</b> Mesh edges & vertices, committed to ModelEnt
+
+<b>Interface(s) used:</b> iGeom, MOAB
+
+<b>Setup:</b>
+
+None.
+
+<b>Notes:</b>
+
+
+This class uses the facets generated for the visualization of solid models in typical CAD 
+software to represent a geometric curve as mesh. Upon execution, this class will call for
+the facet data and store it as part of the ModelEnt's mesh.
+
+*/
