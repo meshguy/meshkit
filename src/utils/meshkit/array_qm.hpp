3ca16129d635ad6eae197b1edfdd0b1438b12ca3
Rajeev Jain <rajeeja@gmail.com>
2016-09-28 17:22:14 -0500

    o Run default parallel mesher with NetGen and AdvFront algorithms. o Moving to C as C++ MPI bindings have been deprecated and are no longer available as default in some custom installations of MPI.

--- src/utils/meshkit/array_qm.hpp
+++ src/utils/meshkit/array_qm.hpp
@@ -1,13 +1,6 @@
-#ifdef __APPLE__
-#include <tr1/array>
-namespace meshkit{
-  using std::tr1::array;
-}
-#endif
 
-#ifdef __linux__
 #include <array>
 namespace meshkit{
   using std::array;
 }
-#endif
+
