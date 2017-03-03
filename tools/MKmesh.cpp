937767e9dbb3176b2fc3e1444c1578f4aa313931
Iulian Grindeanu <iulian@mcs.anl.gov>
2017-03-02 18:17:02 -0600

    add some changes to the MKmesh tool
    do merging and imprinting if 3d bodies
    it will trigger right now only surf meshes
    more work for 3d mesh

--- tools/MKmesh.cpp
+++ tools/MKmesh.cpp
@@ -14,6 +14,7 @@
 
 // MeshKit
 #include "meshkit/MKCore.hpp"
+#include "meshkit/iGeom.hpp"
 #include "meshkit/MeshOp.hpp"
 #include "meshkit/SizingFunction.hpp"
 #include "meshkit/SizingFunctionVar.hpp"
@@ -55,10 +56,38 @@ int main(int argc, char **argv)
     debug = atoi(argv[5]);
   }
 
-  mk->load_geometry(file_name.c_str());
+  //mk->load_geometry(file_name.c_str());
+
+  iGeom* geom = mk->igeom_instance();
+  iGeom::Error ierr = geom->load(file_name.c_str());
+  IBERRCHK(ierr, "Trouble loading the geometry file.");
+  std::vector<iBase_EntityHandle> entities_out, ini_surf, ini_curve, ini_verts;
+  geom->getEntities(0, iBase_REGION, entities_out);
+  geom->getEntities(0, iBase_FACE, ini_surf);
+  geom->getEntities(0, iBase_EDGE, ini_curve);
+  geom->getEntities(0, iBase_VERTEX, ini_verts);
+
+  std::cout << " Reg:" << entities_out.size() << " f:" << ini_surf.size() << " c: " << ini_curve.size()
+		  << " v: " << ini_verts.size() << std::endl;
+  ierr = geom->imprintEnts(&entities_out[0], (int)entities_out.size());
+  IBERRCHK(ierr, "Trouble imprinting ");
+  double dTol = 1.0e-2;
+  ierr = geom->mergeEnts(&entities_out[0],entities_out.size(), dTol);
+  IBERRCHK(ierr, "Trouble merging.");
+  std::vector<iBase_EntityHandle> vols, surfs, curves, verts;
+  geom->getEntities(0, iBase_REGION, vols);
+    geom->getEntities(0, iBase_FACE, surfs);
+    geom->getEntities(0, iBase_EDGE, curves);
+    geom->getEntities(0, iBase_VERTEX, verts);
+
+    std::cout << " after imprint and merge: Reg:" << vols.size() << " f:" << surfs.size() << " c: " << curves.size()
+    		  << " v: " << verts.size() << std::endl;
+
+  // populate the model with model entities
+  mk->populate_model_ents();
   // get the surfaces
   int dim_primary_dim=2; // could change for tet mesher, for example
-  // this tool will work only for surf meshers ...
+  // this tool will work only for surf meshers (now)
   MEntVector primary_ents;
   mk->get_entities_by_dimension(dim_primary_dim, primary_ents);
 
@@ -70,6 +99,7 @@ int main(int argc, char **argv)
   {
     primary_ents[si]->sizing_function_index(sfPtr->core_index());
   }
+  // also set some edges ?
 
   meshOp->set_debug_verbosity(debug);
 
