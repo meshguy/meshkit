e2c00e4d3def328f327c50b4d06f28812ff10adf
Iulian Grindeanu <iulian@mcs.anl.gov>
2017-03-08 10:22:28 -0600

    add an extra argument for dimension of entities to mesh
    also, use a static array for name tag value
    a successful mesh can be made now with something like
    ./MKmesh  fp.brep  AF2DfltTriangleMeshOp 2 0.2 oo.h5m

--- tools/MKmesh.cpp
+++ tools/MKmesh.cpp
@@ -16,6 +16,7 @@
 #include "meshkit/MKCore.hpp"
 #include "meshkit/iGeom.hpp"
 #include "meshkit/MeshOp.hpp"
+#include "meshkit/MeshOpProxy.hpp"
 #include "meshkit/SizingFunction.hpp"
 #include "meshkit/SizingFunctionVar.hpp"
 #include "meshkit/ModelEnt.hpp"
@@ -36,24 +37,32 @@ int main(int argc, char **argv)
 
   std::string file_name, meshop, out_mesh;
   double size = 1.0;
-  int debug=0;
+  int debug=0, mesh_op_dim=3;
 
-  if (argc < 5)
+  if (argc < 6)
   {
-    std::cout <<" usage: " << argv[0] << " <geo_file> <mesh_op> <size> <out_file> [debug_level] \n ";
+    std::cout << " usage: " << argv[0] << " <geo_file> <mesh_op> <mesh_op_dim>[0, 1, 2, 3] <size> <out_file> [debug_level] \n ";
+    // available mesh ops:
+    std::cout << " Available mesh-ops:\n";
+    const unsigned n = MKCore::num_meshops();
+    for (unsigned i = 0; i < n; ++i) {
+      MeshOpProxy* p = MKCore::meshop_proxy(i);
+      std::cout << i << " " << p->name() << "\n";
+    }
     return 1;
   }
-  if (argc >= 5)
+  if (argc >= 6)
   {
     file_name = argv[1];
     meshop = argv[2];
-    size = atof(argv[3]);
-    out_mesh = argv[4];
+    mesh_op_dim = atoi(argv[3]);
+    size = atof(argv[4]);
+    out_mesh = argv[5];
   }
 
-  if (argc>=6)
+  if (argc>=7)
   {
-    debug = atoi(argv[5]);
+    debug = atoi(argv[6]);
   }
 
   //mk->load_geometry(file_name.c_str());
@@ -76,33 +85,37 @@ int main(int argc, char **argv)
   IBERRCHK(ierr, "Trouble merging.");
   std::vector<iBase_EntityHandle> vols, surfs, curves, verts;
   geom->getEntities(0, iBase_REGION, vols);
-    geom->getEntities(0, iBase_FACE, surfs);
-    geom->getEntities(0, iBase_EDGE, curves);
-    geom->getEntities(0, iBase_VERTEX, verts);
+  geom->getEntities(0, iBase_FACE, surfs);
+  geom->getEntities(0, iBase_EDGE, curves);
+  geom->getEntities(0, iBase_VERTEX, verts);
 
-    std::cout << " after imprint and merge: Reg:" << vols.size() << " f:" << surfs.size() << " c: " << curves.size()
-    		  << " v: " << verts.size() << std::endl;
+  std::cout << " after imprint and merge: volumes:" << vols.size() << " faces:" << surfs.size() << " curves: " << curves.size()
+        << " vertices:" << verts.size() << std::endl;
 
   // populate the model with model entities
   mk->populate_model_ents();
+  std::cout <<" model populated successfully with entities\n";
   iGeom::TagHandle nameTag;
-  geom->getTagHandle("NAME", nameTag);
-  for (int dim = 3; dim >= 0; dim--) {
-    MEntVector ents;
-    mk->get_entities_by_dimension(dim, ents);
-    for (unsigned int si = 0u; si < ents.size(); ++si) {
-      ModelEnt * me = ents[si];
-      iGeom::EntityHandle geo_h = me->geom_handle();
-      char * name_ent;
-      geom->getData(geo_h, nameTag, name_ent);
-      std::cout <<" dim: " << dim << " index: " << si << "  name:" << name_ent << "\n";
+  ierr = geom->getTagHandle("NAME", nameTag);
+  if (0 == ierr)
+    for (int dim = 3; dim >= 0; dim--) {
+      MEntVector ents;
+      mk->get_entities_by_dimension(dim, ents);
+      for (unsigned int si = 0u; si < ents.size(); ++si) {
+        ModelEnt * me = ents[si];
+        iGeom::EntityHandle geo_h = me->geom_handle();
+        char name_ent[100];
+        geom->getData(geo_h, nameTag, name_ent);
+        std::cout <<" dim: " << dim << " index: " << si << "  name:" << name_ent << "\n";
+      }
     }
-  }
-  // get the surfaces
-  int dim_primary_dim=2; // could change for tet mesher, for example
+  // get the model entities of interest, for the meshop dim selected
+  // int dim_primary_dim=2; // could change for tet mesher, for example
   // this tool will work only for surf meshers (now)
   MEntVector primary_ents;
-  mk->get_entities_by_dimension(dim_primary_dim, primary_ents);
+  mk->get_entities_by_dimension(mesh_op_dim, primary_ents);
+
+  std::cout<<" mesh using " << meshop << " " << primary_ents.size() << " model entities\n";
 
   // Construct the AF2DfltTriangleMeshOp on the surfaces
   MeshOp * meshOp = mk->construct_meshop(meshop.c_str(), primary_ents);
